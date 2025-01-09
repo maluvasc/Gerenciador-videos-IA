8f5c6ee1bf33fd252e360a1d6b1662ea5ae6cc2b
Malu Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2025-01-08 21:05:36 -0300

    react-router added

---
+++ frontend/src/ui/screens/createRepository/createRepository.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateRepository() {
+    return (
+      <>
+      <h1>Essa é a página de criar repos</h1>
+      </>
+    );
+  }
+  
+  export default CreateRepository;
---
+++ frontend/src/ui/screens/main/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+      <>
+      <h1>Essa é a página home</h1>
+      </>
+    );
+  }
+  
+  export default Home;
