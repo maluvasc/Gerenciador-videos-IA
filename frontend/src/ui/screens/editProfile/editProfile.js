8e942d6dea5f178aed056abdc7288c950f60b95d
Letícia Gomes <leticiagsilvaa@outlook.com>
2025-01-13 23:14:35 -0300

    editProfile changes

--- frontend/src/ui/screens/editProfile/editProfile.js
+++ frontend/src/ui/screens/editProfile/editProfile.js
@@ -3,6 +3,8 @@ import Menu from '../../components/menu/menu';
 import styles from './editProfile.module.css';
 import { useNavigate } from 'react-router-dom';
 import { FiUser } from "react-icons/fi";
+import { FiLock } from 'react-icons/fi';
+import { FiDelete } from 'react-icons/fi';
 
 function EditProfile() {
 
@@ -45,9 +47,52 @@ function EditProfile() {
         </div>
         </div>
         <div className={styles.line}></div>
-        <div className={styles.secondRowFlex}>
+
+        <div className={styles.passwordContainer}>
+            <div className={styles.titleFlex}>
+                <h1 className={styles.titleFont}>Alterar Senha</h1>
+                <FiLock style={{width: "30px", height: "30px"}} />
+            </div>
+            
+            <div className={styles.flexFirstRowSecondColumn}>
+                <div className={styles.userInput}>
+                    <label>Senha Atual</label>
+                    <input className={styles.inputInformationName}></input>
+                </div>
+                <div className={styles.userInput}>
+                    <label>Nova Senha</label>
+                    <input className={styles.inputInformationUser}></input>
+                </div>
+                <div className={styles.userInput}>
+                    <label>Confimar Senha</label>
+                    <input className={styles.inputInformationUser}></input>
+                </div>
+            </div>
+
+            <div className={styles.line}></div>
+        </div>
+        
+        <div className={styles.deleteContainer}>
+            <div className={styles.titleFlex}>
+                    <h1 className={styles.titleFont}>Deletar conta</h1>
+                    <FiDelete style={{width: "30px", height: "30px"}} />
+            </div>
+            
+            
+            <div className={styles.noticeBox}>
+                <p className={styles.noticeText}>
+                Entendo perfeitamente que esta ação é irreversível e, caso eu deseje recuperar o acesso à plataforma no futuro, serei obrigado a enviar uma solicitação formal ao meu administrador de serviço para a criação de uma nova conta.
+                </p>
+            </div>
+            <div class={styles.checkboxContainer}>
+                <input type="checkbox" id="consent" />
+                <label for="consent">Eu concordo com o aviso acima</label>
+            </div>
+            <button class={styles.deleteButton}>Deletar conta permanentemente</button>
+            
             
         </div>
+
         <div className={styles.line}></div>
         </div>
     </>
