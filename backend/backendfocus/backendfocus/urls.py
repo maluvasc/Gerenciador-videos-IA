0ce7b58a1b20e14c95e3bcbd6865fa543666cc8a
gabriel <gabriel.nascimentoramos@ufrpe.br>
2025-02-13 10:38:38 -0300

    pequenos ajustes

--- backend/backendfocus/backendfocus/urls.py
+++ backend/backendfocus/backendfocus/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from app.views import home, CreateUserView, RepositorioListCreate
+from app.views import home, CreateUserView
 
 # Roteamento central do projeto. Ele conecta rotas de aplicativos (como App/urls.py) ao projeto principal.
 
@@ -12,7 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')), # Adicionando a URL do aplicativo
     path('app/user/register/', CreateUserView.as_view(), name='register'), # Adicionando a URL para o registro de usuários
-    path('app/repository/register/', RepositorioListCreate.as_view(), name='repositorio-list'), # Adicionando a URL para o registro de repositorios
     path('app/token/', TokenObtainPairView.as_view(), name='get_token'), # Adicionando a URL para obter o token de acesso
     path('app/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'), # Adicionando a URL para atualizar o token de acesso
     path('app-auth/', include('rest_framework.urls')),
